38ba55f4092bb206702fbea0baf5b3430cd790b6
Prateeksha007 <royalriderr007@gmail.com>
2025-03-28 11:42:06 +0530

    Update java1.java

--- java1.java
+++ java1.java
@@ -5,10 +5,7 @@ public class myjava{
      System.out.println("Hello");
      System.out.println("Hi");
      System.out.println("save it mewrde");
-<<<<<<< HEAD
      System.out.println("Master branch");
-=======
->>>>>>> main
 
     }
 public static void main(String[]args){
