d065cae9d4a547c36831467437ae34f42bffd94c
Anish_Mulla23 <annyaa@gmail.com>
2025-03-28 11:30:28 +0530

    Save it in main

---
+++ java1.java
@@ -0,0 +1,13 @@
+package java;
+
+public class myjava{
+    void print(){
+     System.out.println("Hello");
+     System.out.println("Hi");
+     System.out.println("save it mewrde");
+
+    }
+public static void main(String[]args){
+    myjava obj=new myjava();
+    obj.print();
+}}
