d60b785f0ebe8d2efb38ce23912a748ad8a01537
Anish_Mulla23 <annyaa@gmail.com>
2025-03-28 11:33:59 +0530

    Master the save

---
+++ java1.java
@@ -0,0 +1,14 @@
+package java;
+
+public class myjava{
+    void print(){
+     System.out.println("Hello");
+     System.out.println("Hi");
+     System.out.println("save it mewrde");
+     System.out.println("Master branch");
+
+    }
+public static void main(String[]args){
+    myjava obj=new myjava();
+    obj.print();
+}}
